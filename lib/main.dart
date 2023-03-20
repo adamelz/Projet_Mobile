49ed107bcd2a908535be84d348a4a413792f6ec1
bbernaud <78956868+bbernaud@users.noreply.github.com>
2023-03-20 22:04:18 +0100

    Ajout Firebase authentification

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:projetmobile/screens/Accueil.dart';
 import 'package:projetmobile/screens/Inscription.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -24,11 +31,10 @@ class LoginDemo extends StatefulWidget {
   _LoginDemoState createState() => _LoginDemoState();
 }
 
-class _LoginDemoState extends State<LoginDemo> {
+/*class _LoginDemoState extends State<LoginDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
@@ -40,7 +46,7 @@ class _LoginDemoState extends State<LoginDemo> {
         child: Column(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.only(top:80.0),
+              padding: const EdgeInsets.only(top: 80.0),
               child: Center(
                 child: Container(
                     width: 200,
@@ -48,15 +54,26 @@ class _LoginDemoState extends State<LoginDemo> {
                     /*decoration: BoxDecoration(
                         color: Colors.red,
                         borderRadius: BorderRadius.circular(50.0)),*/
-                    child: Text('Bienvenue !',textAlign: TextAlign.center,style: TextStyle(fontFamily:'Proxima',fontWeight: FontWeight.bold,fontSize: 30,color: Colors.white),)),
-
+                    child: Text(
+                      'Bienvenue !',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: 'Proxima',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          color: Colors.white),
+                    )),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(),
               child: Center(
-                    child: Text("Veuillez vous connecter ou\n créer un nouveau compte\n pour utiliser l'application. ",textAlign: TextAlign.center,style: TextStyle(fontFamily:'Proxima',color: Colors.white),)),
-              ),
+                  child: Text(
+                "Veuillez vous connecter ou\n créer un nouveau compte\n pour utiliser l'application. ",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: 'Proxima', color: Colors.white),
+              )),
+            ),
             SizedBox(height: 50),
             Padding(
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
@@ -68,9 +85,12 @@ class _LoginDemoState extends State<LoginDemo> {
                   fillColor: Color(0xFF1E262C),
 
                   label: const Center(
-                    child: Text("E-mail",style: TextStyle(color: Colors.white),),
+                    child: Text(
+                      "E-mail",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                   ),
+                ),
               ),
             ),
             Padding(
@@ -85,9 +105,12 @@ class _LoginDemoState extends State<LoginDemo> {
                   fillColor: Color(0xFF1E262C),
 
                   label: const Center(
-                    child: Text("Mot de passe",style: TextStyle(color: Colors.white),),
+                    child: Text(
+                      "Mot de passe",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                   ),
+                ),
               ),
             ),
             SizedBox(height: 50),
@@ -95,7 +118,8 @@ class _LoginDemoState extends State<LoginDemo> {
               height: 50,
               width: 350,
               decoration: BoxDecoration(
-                  color: Color(0xFF636af6), borderRadius: BorderRadius.circular(5)),
+                  color: Color(0xFF636af6),
+                  borderRadius: BorderRadius.circular(5)),
               child: TextButton(
                 onPressed: () {
                   Navigator.push(
@@ -109,19 +133,18 @@ class _LoginDemoState extends State<LoginDemo> {
             ),
             SizedBox(height: 15),
             Container(
-
               height: 50,
               width: 350,
               decoration: BoxDecoration(
-                  border: Border.all(width: 3, color:Color(0xFF636af6)),
-                  color: Color(0xFF1a2025), borderRadius: BorderRadius.circular(5)),
+                  border: Border.all(width: 3, color: Color(0xFF636af6)),
+                  color: Color(0xFF1a2025),
+                  borderRadius: BorderRadius.circular(5)),
               child: TextButton(
                 onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => Inscription()));
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => Inscription()));
                 },
                 child: Text(
-
                   'Créer un nouveau compte',
                   style: TextStyle(color: Colors.white, fontSize: 15),
                 ),
@@ -135,6 +158,117 @@ class _LoginDemoState extends State<LoginDemo> {
       ),
     );
   }
+}*/
+class _LoginDemoState extends State<LoginDemo> {
+  final _auth = FirebaseAuth.instance;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future<void> _handleSignIn() async {
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      print('User ${userCredential.user?.uid} signed in');
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => HomePage()),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        // create user if not found
+        UserCredential userCredential =
+        await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        print('User ${userCredential.user?.uid} created');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => HomePage()),
+        );
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            // ...
+            SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                controller: _emailController,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF1E262C),
+                  label: const Center(
+                    child: Text(
+                      "E-mail",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+              const EdgeInsets.only(left: 25.0, right: 25.0, top: 15),
+              child: TextField(
+                controller: _passwordController,
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF1E262C),
+                  label: const Center(
+                    child: Text(
+                      "Mot de passe",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            Container(
+              height: 50,
+              width: 350,
+              decoration: BoxDecoration(
+                  color: Color(0xFF636af6),
+                  borderRadius: BorderRadius.circular(5)),
+              child: TextButton(
+                onPressed: () {
+                  _handleSignIn();
+                },
+                child: Text(
+                  'Se connecter',
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+              ),
+            ),
+            // ...
+          ],
+        ),
+      ),
+    );
+  }
 }
-
-
